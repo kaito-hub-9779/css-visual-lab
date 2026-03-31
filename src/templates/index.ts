08932d71a45e08b14034e51b2157b160884ac460
kaito-hub-9779 <179327100+kaito-hub-9779@users.noreply.github.com>
2026-03-31 02:06:21 +0000

    feat: add daily CSS template [auto]

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,3 +1,4 @@
+import { gridAutoRowsTemplate } from "./gridAutoRows";
 import { maskSizeTemplate } from "./maskSize";
 import { maskModeTemplate } from "./maskMode";
 import { textWrapTemplate } from "./textWrap";
@@ -91,6 +92,7 @@ import { zIndexTemplate } from "../templates/zIndex";
 import type { PropertyTemplate } from "../types";
 
 export const templates: PropertyTemplate[] = [
+  gridAutoRowsTemplate,
   maskSizeTemplate,
   maskModeTemplate,
   textWrapTemplate,
